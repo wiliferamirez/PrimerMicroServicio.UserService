077860dc4b7b711f5c24a0da6641540f786b10cf
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-06-09 21:35:34 -0500

    Add fields that user sends for registration

--- PrimerMicroServicio/Application/DTOs/RegisterUserDto.cs
+++ PrimerMicroServicio/Application/DTOs/RegisterUserDto.cs
@@ -1,12 +1,10 @@
-namespace PrimerMicroServicio.Domain.Entities;
+namespace PrimerMicroServicio.Application.DTOs;
 
-public class User
+public class RegisterUserDto
 {
-    public Guid IdUser { get; set; }
     public string CedulaUser { get; set; }
     public string UserFirstName { get; set; }
     public string UserLastName { get; set; }
     public string UserPasswordHash { get; set; }
     public string UserEmail { get; set; }
-    public DateTime UserRegisterDate { get; set; }
 }
