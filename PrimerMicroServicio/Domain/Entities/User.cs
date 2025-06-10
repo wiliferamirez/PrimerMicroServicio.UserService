362c85203a8fea1ef77933cdd7b925325e83c89d
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-06-09 21:27:30 -0500

    Add User model attributes

--- PrimerMicroServicio/Domain/Entities/User.cs
+++ PrimerMicroServicio/Domain/Entities/User.cs
@@ -2,5 +2,11 @@ namespace PrimerMicroServicio.Domain.Entities;
 
 public class User
 {
-    
+    public string IdUser { get; set; }
+    public string CedulaUser { get; set; }
+    public string UserFirstName { get; set; }
+    public string UserLastName { get; set; }
+    public string UserPasswordHash { get; set; }
+    public string UserEmail { get; set; }
+    public DateTime UserRegisterDate { get; set; }
 }
