a18a2341a49d23ad0d0bec34ba68121609a4e75d
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-06-09 22:35:05 -0500

    Add Entity method to Dto

--- PrimerMicroServicio/Application/DTOs/UserDto.cs
+++ PrimerMicroServicio/Application/DTOs/UserDto.cs
@@ -5,7 +5,16 @@ namespace PrimerMicroServicio.Application.DTOs;
 public class UserDto
 {
     public Guid IdUser { get; set; }
-    public string UserFirstName { get; set; }
-    public string UserLastName { get; set; }
+    public string UserLongName{ get; set; }
     public string UserEmail { get; set; }
+
+    public static UserDto FromEntity(User user)
+    {
+        return new UserDto
+        {
+            IdUser = user.IdUser,
+            UserLongName = $"{user.UserFirstName} {user.UserLastName}",
+            UserEmail = user.UserEmail
+        };
+    }
 }
