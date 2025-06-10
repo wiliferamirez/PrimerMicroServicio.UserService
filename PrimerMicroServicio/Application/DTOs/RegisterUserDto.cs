602b522cec82172431e460af29aacdded215d756
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-06-09 21:43:05 -0500

    Add dataannotations for validation

--- PrimerMicroServicio/Application/DTOs/RegisterUserDto.cs
+++ PrimerMicroServicio/Application/DTOs/RegisterUserDto.cs
@@ -1,10 +1,24 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace PrimerMicroServicio.Application.DTOs;
 
 public class RegisterUserDto
 {
+    [Required]
+    [StringLength(10)]
+    [RegularExpression("^[0-9]*$", ErrorMessage = "Only numeric characters allowed")]
     public string CedulaUser { get; set; }
+    [Required]
+    [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", ErrorMessage = "Numeric characters not allowed")]
     public string UserFirstName { get; set; }
+    [Required]
+    [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", ErrorMessage = "Numeric characters not allowed")]
     public string UserLastName { get; set; }
+    [Required]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
     public string UserPasswordHash { get; set; }
+    [Required]
+    [EmailAddress(ErrorMessage = "Email address not valid")]
     public string UserEmail { get; set; }
 }
