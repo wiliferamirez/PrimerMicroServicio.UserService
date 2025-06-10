35009d0832154d057f4e9b8ce21b479dcf65be36
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-06-09 22:01:28 -0500

    Create UserDto in order to expose harmless data from user

--- PrimerMicroServicio/Application/DTOs/UserDto.cs
+++ PrimerMicroServicio/Application/DTOs/UserDto.cs
@@ -1,6 +1,11 @@
+using PrimerMicroServicio.Domain.Entities;
+
 namespace PrimerMicroServicio.Application.DTOs;
 
 public class UserDto
 {
-    
+    public Guid IdUser { get; set; }
+    public string UserFirstName { get; set; }
+    public string UserLastName { get; set; }
+    public string UserEmail { get; set; }
 }
