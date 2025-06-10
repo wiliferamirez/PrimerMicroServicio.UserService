d0ccac3cab36d927a60530cc517b3749faeef2c1
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-06-09 22:53:50 -0500

    Add endpoints REST by using ControllerBase

--- PrimerMicroServicio/Program.cs
+++ PrimerMicroServicio/Program.cs
@@ -1,3 +1,6 @@
+using PrimerMicroServicio.Application.Interfaces;
+using PrimerMicroServicio.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
